204f62064b9822f1706d6f6224b13974b3b6a532
Ericpfeuti <eric.pfeuti2007@gmail.com>
2025-06-18 09:25:25 -0300

    Aula 2 - Games

--- SunnyLand/Assets/Scripts/Coletavel.cs
+++ SunnyLand/Assets/Scripts/Coletavel.cs
@@ -1,20 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Coletavel : MonoBehaviour
 {
-    // Debug.Log = console.log();
-
-    private int pontos = 5;
-
-    void Start()
+    public TMP_Text texto_pontos;
+    public ParticleSystem efeito;
+    int pontos = 0;
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("O jogo começou com " + pontos + " pontos."); // Concatenar;
+        if (collision.CompareTag("Player"))
+        {
+            pontos++;
+            texto_pontos.text = "Pontos: " + pontos.ToString();
+            collision.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+            Instantiate(efeito, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }    
     }
 
-    void Update()
-    {
-        // Debug.Log("Rodando!");
-    }
+
 }
--- SunnyLand/Assets/Scripts/Personagem.cs
+++ SunnyLand/Assets/Scripts/Personagem.cs
@@ -4,19 +4,10 @@ using UnityEngine;
 
 public class Personagem : MonoBehaviour
 {
-    private string name = "Caio";
-    private int health = 100;
-    private float speed = 2.5f;
-    private bool alive = true;
 
-    public int vida = 5;
-    private Rigidbody2D rb;
-    public float novaGravidade = 1.5f;
     void Start()
     {
-        Debug.Log("O personagem " + name + ", tem " + health + " de vida e " + speed + " de velocidade.\n Portanto, a propriedade de sua vida é " + alive + ";");
-        rb = GetComponent<Rigidbody2D>();
-        rb.gravityScale = novaGravidade;
+
     }
 
     void Update()
